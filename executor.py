41124a412db92ad62a2e0772d20f4fe881729a02
Florian Hönicke <hoenicke.florian@gmail.com>
2021-12-21 00:40:51 +0100

    Update executor.py

--- executor.py
+++ executor.py
@@ -6,4 +6,4 @@ class ImageUriToBlob(Executor):
     def convert_image_uri_to_blob(self, docs: DocumentArray, **kwargs):
         for doc in docs:
             doc.tags["uri"] = doc.uri # So we don't lose access to uri
-            doc.convert_image_uri_to_blob()
+            doc.convert_uri_to_image_blob()
